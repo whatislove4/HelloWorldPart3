1dbede92a2008a26b127c5df72aa3cc2fcc03114
whatislove4 <lokotunin12@mail.ru>
2025-05-19 20:10:51 +0300

    Initial Commit

---
+++ HelloWorldPart3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldPart3
+//
+//  Created by Игнат Гончаров on 19.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
